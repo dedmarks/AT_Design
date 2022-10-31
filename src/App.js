ae94212c3d06a540110509b6dcc0340b756542de
dedmarks <dedmark99@gmail.com>
2022-10-31 21:22:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Box from './Components/Box';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <div className="bg"/>
+      <Box/>
+    </div>
+  );
+}
+
+export default App;
