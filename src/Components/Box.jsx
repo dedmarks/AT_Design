c7b5ef163c074d9ba5a4e35c447822a876526c30
dedmarks <dedmark99@gmail.com>
2022-11-02 13:38:58 +0100

    first commit

--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const Box = () => {
   return (
     <div className="Box">
-        
+        <div className="left_line"/>
+        <div className="row">
+            <h1 className="top_txt">Stworzymy towją przetrzeń</h1>
+            <h1 className="main_txt">Projektowanie<br />Wnętrz</h1>
+            <div className="txt_container">
+            <img className="arrow_down" src="https://static.thenounproject.com/png/1512370-200.png"/>
+                <h3 className="sub_txt">Portfolio</h3>
+            </div>
+        </div>
+        <div className="right_box">
+            <h1 className="l">01</h1>
+        <div className="txt_container1">
+            <h1 className="contact_txt">Kontakt</h1>
+            <img className="arrow_right" src="https://static.thenounproject.com/png/1512370-200.png"/>
+            </div>
+        </div>
     </div>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,6 +4,11 @@ const Header = () => {
   return (
     <div className="header">
         <h1 className="logo">AT Design</h1>
+        <div className="nav_container">
+            <li>ok</li>
+            <li>li</li>
+            <li>ok</li>
+        </div>
     </div>
   )
 }
